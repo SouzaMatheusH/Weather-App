8b6351a802a982895758a962bad27d3415d97cb4
SouzaMatheusH <matheussouzahs@gmail.com>
2024-09-14 19:09:57 -0300

    Aplicação de climas enviada.

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "597ed0dcbemsh8c50d7626daddb0p1bcdccjsn144edef198e1",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3837f9b7a0f09376b49ec520c1287fb0"; // enter your key from openweather API
